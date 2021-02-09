ae8205aba0fb37363f12169b3d7b7600a9f97a37
Chromium7 <christofersaputra29@gmail.com>
2021-02-09 15:51:59 +0700

    Added index page and routing url and views

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,6 @@ app_name = "chat"
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('chat/', views.chat_redirect, name="chat_redirect"),
     path('chat/<int:room_id>/', views.chat_room, name="chat_room"),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 
 def index(request):
     return render(request, 'chat/index.html')
+
+
+@require_POST
+def chat_redirect(request):
+    room_id = int(request.POST['room_id'])
+    return redirect('chat:chat_room', room_id)
 
 
 def chat_room(request, room_id):

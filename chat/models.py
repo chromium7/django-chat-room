40926f347c1b144240c54d056492aa98d392a053
Chromium7 <christofersaputra29@gmail.com>
2021-02-12 15:11:17 +0700

    Create database models for chat room and messages

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Room(models.Model):
+    """
+    Model for chat room.
+    Each room will have a creator and moderators.
+    Each room can either be locked or not using password
+    """    
+    creator =  models.ForeignKey(User, on_delete=models.CASCADE, related_name='rooms_created')
+    password = models.CharField(max_length=50, default=None, blank=True)
+    moderator = models.ManyToManyField(User)
+
+
+class Message(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_logs')
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='chat_logs')
+    message = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)

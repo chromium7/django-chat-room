6cfd0349e8c906f94d8a11bd61188be2705e2ef9
Chromium7 <christofersaputra29@gmail.com>
2021-02-16 19:28:58 +0700

    Add database connection

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
 
 # Redis Channels config
 # https://channels.readthedocs.io/en/stable/topics/channel_layers.html
